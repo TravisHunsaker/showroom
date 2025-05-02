6d5c95362618d0fc09a198bddde6ba8791154332
Travis Hunsaker <travhunsaker@yahoo.com>
2025-05-02 15:36:34 -0600

    Edited stles for details page

---
+++ frontend/src/components/NavBar.svelte
@@ -0,0 +1,8 @@
+<script></script>
+<div class="absolute flex flex-col gap-6 h-full justify-center items-start w-full font-bold z-1 text-xl pl-10">
+    <button>Movies</button>
+    <button>Watchlist</button>
+    <button>Reviews</button>
+</div>
+
+<style></style>
--- frontend/src/routes/movies/[movieId]/+page.svelte
+++ frontend/src/routes/movies/[movieId]/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+	import NavBar from '../../../components/NavBar.svelte';
 	import ReviewModal from '../../../components/ReviewModal.svelte';
 
 	export let data;
@@ -12,25 +13,26 @@
 	};
 </script>
 
-<div class="relative flex h-screen items-center justify-center bg-[#00131C] p-10 text-white">
-	<div class="flex w-[1000px] gap-10">
+<div class="relative flex h-screen items-center justify-center bg-black p-10 text-white">
+    <NavBar />
+    <img
+        class="absolute h-full w-full rounded-xl object-cover opacity-50 z-0 blur-xs"
+        src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
+        alt={movie.title}
+    />
+	<div class="flex w-[1000px] gap-10 z-1">
 		<img
-			class="w-[400px] rounded-xl object-contain"
+			class="w-[400px] rounded-xl object-contain border-white border-3"
 			src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
 			alt={movie.title}
 		/>
-		<div class="relative flex flex-1">
-			<img
-				class="absolute h-full w-full rounded-xl object-cover opacity-50"
-				src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
-				alt={movie.title}
-			/>
+		<div class="relative flex flex-1 border-white border-3 rounded-xl bg-black/50 backdrop-blur-xs">
 			<div class="z-1 flex flex-col justify-between p-6">
 				<div class="flex flex-col gap-4">
 					<h1 class="text-2xl font-bold">{movie.title}</h1>
 					<div class="flex gap-2">
 						{#each movie.genres as genre}
-							<div class="px-4 py-1 rounded-full bg-black/75">{genre.name}</div>
+							<div class="px-4 py-1 rounded-full bg-white/25 font-bold backdrop-blur-xs">{genre.name}</div>
 						{/each}
 					</div>
 					<p>{movie.overview}</p>
